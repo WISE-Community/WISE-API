d7ccb698b6ec6103384753b41bf1da777bbbcae7
Geoffrey Kwan <geoffreykwan@gmail.com>
2021-05-17 17:14:15 -0400

    Update api useful shortcuts.

--- scripts/beforeInstall.sh
+++ scripts/beforeInstall.sh
@@ -4,7 +4,6 @@ export HOME=/home/ubuntu
 export BUILD_DIR=$HOME/build-folder
 export BUILD_FILES=$HOME/wise-build-files
 export CATALINA_HOME=/var/lib/tomcat9
-export env=prod
 
 sudo -u ubuntu -g ubuntu touch $HOME/deploy.log
 exec &>> $HOME/deploy.log
@@ -52,7 +51,7 @@ echo "Add https to Tomcat server.xml"
 sed 's/<Connector port="8080"/<Connector port="8080" scheme="https"/' -i $CATALINA_HOME/conf/server.xml
 
 echo "Copying setenv.sh file to Tomcat bin folder"
-cp $BUILD_FILES/setenv.sh /usr/share/tomcat9/bin/setenv.sh
+cp $BUILD_FILES/api/setenv.sh /usr/share/tomcat9/bin/setenv.sh
 
 echo "Restarting Tomcat"
 service tomcat9 restart
@@ -75,7 +74,7 @@ sed 's/gzip on;/gzip on;\n        gzip_types text\/plain text\/xml image\/gif im
 
 echo "Copying WISE Nginx config file to Nginx sites-enabled folder"
 rm -f /etc/nginx/sites-enabled/*
-cp $BUILD_FILES/$env/api/wise.conf /etc/nginx/sites-enabled/wise.conf
+cp $BUILD_FILES/api/wise.conf /etc/nginx/sites-enabled/wise.conf
 systemctl restart nginx
 
 echo "Creating additional folders for WISE"
@@ -84,26 +83,24 @@ sudo -u ubuntu -g ubuntu mkdir $HOME/backup
 sudo -u ubuntu -g tomcat mkdir $HOME/googleTokens
 
 echo "Copying application.properties file to the build folder"
-cp $BUILD_FILES/$env/api/application.properties $BUILD_DIR/WEB-INF/classes/application.properties
+cp $BUILD_FILES/api/application.properties $BUILD_DIR/WEB-INF/classes/application.properties
 
 echo "Installing network drive package"
 apt-get install nfs-common -y
 
 echo "Mounting network drive folders"
-cp $BUILD_FILES/$env/api/fstab /etc/fstab
+cp $BUILD_FILES/api/fstab /etc/fstab
 mount -a
 
 echo "Copying .vimrc file to the ubuntu home folder"
 sudo -u ubuntu -g ubuntu cp $BUILD_FILES/.vimrc $HOME/.vimrc
 
 echo "Appending text to .bashrc"
-cat $BUILD_FILES/append-to-bashrc.txt >> ~/.bashrc
-cat $BUILD_FILES/$env/api/append-to-bashrc.txt >> ~/.bashrc
+cat $BUILD_FILES/api/append-to-bashrc.txt >> ~/.bashrc
 source ~/.bashrc
 
 echo "Copying message of the day file to update-motd.d folder to display notes on login"
-cp $BUILD_FILES/99-notes /etc/update-motd.d/99-notes
-cat $BUILD_FILES/$env/api/append-to-99-notes.txt >> /etc/update-motd.d/99-notes
+cp $BUILD_FILES/api/99-notes /etc/update-motd.d/99-notes
 chmod 755 /etc/update-motd.d/99-notes
 
 echo "Install mysql client"
