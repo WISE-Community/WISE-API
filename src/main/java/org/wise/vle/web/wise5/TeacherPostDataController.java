d9fc43693de2abbf6c58a1dee5a00c14354a4f5c
Hiroki Terashima <honchikun@gmail.com>
2021-12-16 10:38:31 -0800

    refactor(Teacher Data): Split TeacherDataController
    - Split into TeacherGet/PostDataController
    - use RunImpl, WorkgroupImpl, ProjectImpl domain instead of Long

--- src/main/java/org/wise/vle/web/wise5/TeacherPostDataController.java
+++ src/main/java/org/wise/vle/web/wise5/TeacherPostDataController.java
@@ -3,8 +3,6 @@ package org.wise.vle.web.wise5;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Calendar;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Set;
 
 import javax.servlet.http.HttpServletResponse;
@@ -15,135 +13,46 @@ import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.wise.portal.dao.ObjectNotFoundException;
-import org.wise.portal.domain.project.Project;
-import org.wise.portal.domain.run.Run;
+import org.wise.portal.domain.project.impl.ProjectImpl;
+import org.wise.portal.domain.run.impl.RunImpl;
 import org.wise.portal.domain.user.User;
 import org.wise.portal.presentation.web.controllers.ControllerUtil;
-import org.wise.portal.service.project.ProjectService;
-import org.wise.portal.service.run.RunService;
 import org.wise.portal.service.vle.wise5.VLEService;
 import org.wise.portal.spring.data.redis.MessagePublisher;
 import org.wise.vle.domain.annotation.wise5.Annotation;
 import org.wise.vle.domain.notification.Notification;
 import org.wise.vle.domain.work.Event;
-import org.wise.vle.domain.work.StudentWork;
 
-/**
- * Controller for handling GET and POST of WISE5 Teacher related data like annotations.
- */
 @Secured("ROLE_TEACHER")
-@Controller("wise5TeacherDataController")
-public class TeacherDataController {
+@Controller
+public class TeacherPostDataController {
 
   @Autowired
   private VLEService vleService;
 
   @Autowired
-  private ProjectService projectService;
-
-  @Autowired
-  private RunService runService;
-
-  @Autowired
   private MessagePublisher redisPublisher;
 
-  @GetMapping("/api/teacher/data")
-  @ResponseBody
-  protected HashMap<String, Object> getWISE5TeacherData(
-      @RequestParam(value = "getStudentWork", defaultValue = "false") boolean getStudentWork,
-      @RequestParam(value = "getEvents", defaultValue = "false") boolean getEvents,
-      @RequestParam(value = "getAnnotations", defaultValue = "false") boolean getAnnotations,
-      @RequestParam(value = "id", required = false) Integer id,
-      @RequestParam(value = "runId", required = false) Integer runId,
-      @RequestParam(value = "periodId", required = false) Integer periodId,
-      @RequestParam(value = "workgroupId", required = false) Integer workgroupId,
-      @RequestParam(value = "isAutoSave", required = false) Boolean isAutoSave,
-      @RequestParam(value = "isSubmit", required = false) Boolean isSubmit,
-      @RequestParam(value = "nodeId", required = false) String nodeId,
-      @RequestParam(value = "componentId", required = false) String componentId,
-      @RequestParam(value = "componentType", required = false) String componentType,
-      @RequestParam(value = "context", required = false) String context,
-      @RequestParam(value = "category", required = false) String category,
-      @RequestParam(value = "event", required = false) String event,
-      @RequestParam(value = "fromWorkgroupId", required = false) Integer fromWorkgroupId,
-      @RequestParam(value = "toWorkgroupId", required = false) Integer toWorkgroupId,
-      @RequestParam(value = "studentWorkId", required = false) Integer studentWorkId,
-      @RequestParam(value = "localNotebookItemId", required = false) String localNotebookItemId,
-      @RequestParam(value = "notebookItemId", required = false) Integer notebookItemId,
-      @RequestParam(value = "annotationType", required = false) String annotationType,
-      @RequestParam(value = "components", required = false) List<JSONObject> components,
-      @RequestParam(value = "onlyGetLatest", required = false) Boolean onlyGetLatest) {
-    HashMap<String, Object> data = new HashMap<String, Object>();
-    try {
-      User signedInUser = ControllerUtil.getSignedInUser();
-      Run run = runService.retrieveById(new Long(runId));
-      User owner = run.getOwner();
-      Set<User> sharedOwners = run.getSharedowners();
-      if (owner.equals(signedInUser) || sharedOwners.contains(signedInUser)
-          || signedInUser.isAdmin()) {
-        if (getStudentWork) {
-          List<StudentWork> studentWorkList = vleService.getStudentWorkList(id, runId, periodId,
-              workgroupId, isAutoSave, isSubmit, nodeId, componentId, componentType, components,
-              onlyGetLatest);
-          data.put("studentWorkList", studentWorkList);
-        }
-        if (getEvents) {
-          List<Event> events = vleService.getEvents(id, runId, periodId, workgroupId, nodeId,
-              componentId, componentType, context, category, event, components);
-          data.put("events", events);
-        }
-        if (getAnnotations) {
-          List<Annotation> annotations = vleService.getAnnotations(id, runId, periodId,
-              fromWorkgroupId, toWorkgroupId, nodeId, componentId, studentWorkId,
-              localNotebookItemId, notebookItemId, annotationType);
-          data.put("annotations", annotations);
-        }
-      }
-    } catch (ObjectNotFoundException onfe) {
-      onfe.printStackTrace();
-    }
-    return data;
-  }
-
   @PostMapping("/api/teacher/data")
-  public void postWISETeacherData(HttpServletResponse response,
-      @RequestParam(value = "workgroupId", required = false) Integer workgroupId,
-      @RequestParam(value = "projectId", required = false) Integer projectId,
-      @RequestParam(value = "runId", required = false) Integer runId,
+  public void postData(HttpServletResponse response,
+      @RequestParam(value = "projectId", required = false) ProjectImpl project,
+      @RequestParam(value = "runId", required = false) RunImpl run,
       @RequestParam(value = "annotations", required = false) String annotations,
       @RequestParam(value = "events", required = false) String events) {
     JSONObject result = new JSONObject();
     try {
       User signedInUser = ControllerUtil.getSignedInUser();
-      Project project = null;
-      Run run = null;
       User owner = null;
-      Integer userId = null;
       Set<User> sharedOwners = null;
-      if (runId != null) {
-        run = runService.retrieveById(new Long(runId));
-        if (run != null) {
-          owner = run.getOwner();
-          sharedOwners = run.getSharedowners();
-        }
+      if (run != null) {
+        owner = run.getOwner();
+        sharedOwners = run.getSharedowners();
       }
-      if (projectId != null) {
-        project = projectService.getById(new Long(projectId));
-        if (project != null) {
-          owner = project.getOwner();
-          sharedOwners = project.getSharedowners();
-        }
-      }
-      if (signedInUser != null) {
-        Long userIdLong = signedInUser.getId();
-        if (userIdLong != null) {
-          userId = userIdLong.intValue();
-        }
+      if (project != null) {
+        owner = project.getOwner();
+        sharedOwners = project.getSharedowners();
       }
       /*
        * the signed in user is an owner of the project or run or we are saving a teacher event that
@@ -151,7 +60,7 @@ public class TeacherDataController {
        */
       if ((owner != null && owner.equals(signedInUser))
           || (sharedOwners != null && sharedOwners.contains(signedInUser))
-          || (runId == null && projectId == null && events != null)) {
+          || (run == null && project == null && events != null)) {
         if (annotations != null) {
           JSONArray annotationsJSONArray = new JSONArray(annotations);
           if (annotationsJSONArray != null) {
@@ -232,7 +141,7 @@ public class TeacherDataController {
                       : eventJSONObject.getString("clientSaveTime"),
                     eventJSONObject.isNull("projectId") ? null
                       : eventJSONObject.getInt("projectId"),
-                    userId);
+                    signedInUser.getId().intValue());
 
                 // before returning saved Event, strip all fields except id,
                 // responseToken, and serverSaveTime to minimize response size
@@ -248,8 +157,6 @@ public class TeacherDataController {
           }
         }
       }
-    } catch (ObjectNotFoundException e) {
-      e.printStackTrace();
     } catch (JSONException e) {
       e.printStackTrace();
     }
@@ -277,8 +184,7 @@ public class TeacherDataController {
   /**
    * Saves and returns a notification for the specified annotation
    *
-   * @param annotation
-   *                     Annotation to create the notification for
+   * @param annotation  Annotation to create the notification for
    * @return Notification notification for the specified annotation
    */
   private Notification createNotificationForAnnotation(Annotation annotation) {
