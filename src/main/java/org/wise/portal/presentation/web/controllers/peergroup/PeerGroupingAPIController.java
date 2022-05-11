1c27adb31e09ade9b86ac66b64ab43523627c276
Geoffrey Kwan <geoffreykwan@gmail.com>
2022-05-11 15:58:07 -0700

    fix(Peer Grouping): Return bad request when creating duplicate tag
    #37

--- src/main/java/org/wise/portal/presentation/web/controllers/peergroup/PeerGroupingAPIController.java
+++ src/main/java/org/wise/portal/presentation/web/controllers/peergroup/PeerGroupingAPIController.java
@@ -1,6 +1,7 @@
 package org.wise.portal.presentation.web.controllers.peergroup;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 import org.wise.portal.domain.peergrouping.PeerGrouping;
 import org.wise.portal.domain.peergrouping.impl.PeerGroupingImpl;
 import org.wise.portal.domain.run.impl.RunImpl;
@@ -26,7 +28,11 @@ public class PeerGroupingAPIController {
   @PreAuthorize("hasPermission(#run, 'WRITE') or hasRole('ROLE_ADMINISTRATOR')")
   PeerGrouping create(@PathVariable("runId") RunImpl run,
       @RequestBody PeerGroupingImpl peerGrouping) {
-    return peerGroupingService.createPeerGrouping(run, peerGrouping);
+    try {
+      return peerGroupingService.createPeerGrouping(run, peerGrouping);
+    } catch (Exception e) {
+      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Duplicate Tag");
+    }
   }
 
   @PutMapping("/{tag}")
