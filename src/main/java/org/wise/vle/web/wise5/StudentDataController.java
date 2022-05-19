3922688b802a8468fe6c012264587ea41d23017f
Hiroki Terashima <honchikun@gmail.com>
2022-05-19 12:50:26 -0700

    feat(Security): Return 403 status when access is denied
    Also secure StudentDataController methods to ROLE_USER

--- src/main/java/org/wise/vle/web/wise5/StudentDataController.java
+++ src/main/java/org/wise/vle/web/wise5/StudentDataController.java
@@ -36,6 +36,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -60,6 +61,7 @@ import org.wise.vle.domain.work.StudentWork;
  *
  * @author Hiroki Terashima
  */
+@Secured("ROLE_USER")
 @Controller("wise5StudentDataController")
 @RequestMapping("/api")
 public class StudentDataController {
