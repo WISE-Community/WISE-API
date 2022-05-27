000f4be42f68b25ee223f4e3f7cabae3d7d9cb52
Geoffrey Kwan <geoffreykwan@gmail.com>
2022-05-27 15:55:49 -0700

    refactor(Websocket): Change websocket endpoint protocol from http to ws
    #136

--- src/main/java/org/wise/portal/spring/impl/WebSocketConfig.java
+++ src/main/java/org/wise/portal/spring/impl/WebSocketConfig.java
@@ -25,7 +25,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
-    registry.addEndpoint("/websocket").setAllowedOriginPatterns("*").withSockJS();
+    registry.addEndpoint("/websocket").setAllowedOriginPatterns("*");
   }
 
   @Override
