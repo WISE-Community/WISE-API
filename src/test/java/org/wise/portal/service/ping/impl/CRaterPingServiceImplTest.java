70500f085e00c968bf9d34249ccecb74fdce0ac2
Aaron Detre <aarondetre@gmail.com>
2025-05-15 12:42:16 -0700

    Fix CRaterPingService tests

--- src/test/java/org/wise/portal/service/ping/impl/CRaterPingServiceImplTest.java
+++ src/test/java/org/wise/portal/service/ping/impl/CRaterPingServiceImplTest.java
@@ -2,18 +2,21 @@ package org.wise.portal.service.ping.impl;
 
 import static org.easymock.EasyMock.expect;
 import static org.easymock.EasyMock.replay;
+import static org.easymock.EasyMock.verify;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.verify;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.time.Duration;
 import java.util.HashSet;
 import java.util.Set;
 
 import org.easymock.EasyMockExtension;
 import org.easymock.Mock;
 import org.easymock.TestSubject;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.data.redis.core.SetOperations;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 @ExtendWith(EasyMockExtension.class)
@@ -24,39 +27,43 @@ public class CRaterPingServiceImplTest {
   @Mock
   private StringRedisTemplate stringRedisTemplate;
 
+  @Mock
+  private SetOperations<String, String> setOperations;
+
   private String testId = "test";
 
-  // @Test
-  // public void hasPingedItem_ItemPinged_ShouldReturnTrue() {
-  //   Set<String> members = new HashSet<String>();
-  //   members.add("pinged");
-  //   expect(stringRedisTemplate.opsForSet().members(testId)).andReturn(members);
-  //   replay(stringRedisTemplate);
-  //   assertTrue(pingEndpointServiceImpl.hasPingedItem(testId));
-  //   verify(stringRedisTemplate);
-  // }
+  @Test
+  public void hasPingedItem_ItemPinged_ShouldReturnTrue() {
+    Set<String> members = new HashSet<String>();
+    members.add(testId);
+    expect(stringRedisTemplate.opsForSet()).andReturn(setOperations);
+    expect(setOperations.members(testId)).andReturn(members);
+    replay(stringRedisTemplate, setOperations);
+    assertTrue(pingEndpointServiceImpl.hasPingedItem(testId));
+    verify(stringRedisTemplate);
+    verify(setOperations);
+  }
  
-  // @Test
-  // public void hasPingedItem_ItemNotPinged_ShouldReturnFalse() {
-  //   expect(stringRedisTemplate.opsForSet().members(testId)).andReturn(new HashSet<String>());
-  //   assertFalse(pingEndpointServiceImpl.hasPingedItem(testId));
-  // }
+  @Test
+  public void hasPingedItem_ItemNotPinged_ShouldReturnFalse() {
+    Set<String> members = new HashSet<String>();
+    expect(stringRedisTemplate.opsForSet()).andReturn(setOperations);
+    expect(setOperations.members(testId)).andReturn(members);
+    replay(stringRedisTemplate, setOperations);
+    assertFalse(pingEndpointServiceImpl.hasPingedItem(testId));
+    verify(stringRedisTemplate);
+    verify(setOperations);
+
+  }
 
 
-  // @Test
-  // public void cachePingedItem_ShouldCacheAndExpireItemId() {
-  //   assertTrue(stringRedisTemplateOpsSize(testId) == 0);
-  //   pingEndpointServiceImpl.cachePingedItem(testId, 1);
-  //   assertTrue(stringRedisTemplateOpsSize(testId) == 1);
-  //   try { 
-  //     Thread.sleep(1001);
-  //   } catch(InterruptedException e) {
-  //     e.printStackTrace();
-  //   }
-  //   assertTrue(stringRedisTemplateOpsSize(testId) == 0);
-  // }
-
-  // private int stringRedisTemplateOpsSize(String id) {
-  //   return stringRedisTemplate.opsForSet().members(id).size();
-  // }
+  @Test
+  public void cachePingedItem_ShouldCacheAndExpireItemId() {
+    expect(stringRedisTemplate.opsForSet()).andReturn(setOperations);
+    expect(stringRedisTemplate.expire(testId, Duration.ofSeconds(1))).andReturn(null);
+    expect(setOperations.add(testId, "pinged")).andReturn(null);
+    replay(stringRedisTemplate, setOperations);
+    pingEndpointServiceImpl.cachePingedItem(testId, 1);
+    verify(stringRedisTemplate, setOperations);
+  }
 }
