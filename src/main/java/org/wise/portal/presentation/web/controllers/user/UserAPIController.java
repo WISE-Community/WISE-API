4d2bd2d4da6a6dfafec4105d6f4e617c37ee8a65
Aaron Detre <aarondetre@gmail.com>
2025-05-21 15:36:54 -0700

    Send isSurvey data back to the client with the rest of the run data

--- src/main/java/org/wise/portal/presentation/web/controllers/user/UserAPIController.java
+++ src/main/java/org/wise/portal/presentation/web/controllers/user/UserAPIController.java
@@ -305,6 +305,7 @@ public class UserAPIController {
     HashMap<String, Object> map = new HashMap<String, Object>();
     map.put("id", run.getId());
     map.put("name", run.getName());
+    map.put("isSurvey", run.getIsSurvey());
     map.put("maxStudentsPerTeam", run.getMaxWorkgroupSize());
     map.put("runCode", run.getRuncode());
     map.put("startTime", run.getStartTimeMilliseconds());
