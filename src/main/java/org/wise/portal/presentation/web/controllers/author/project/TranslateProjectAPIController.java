a262a332151d27b31da35fb92d0c1b8e48055da0
Aaron Detre <aarondetre@gmail.com>
2026-03-13 15:02:19 -0400

    Handle missing app props, created TranslatableText class, and other suggested changes

--- src/main/java/org/wise/portal/presentation/web/controllers/author/project/TranslateProjectAPIController.java
+++ src/main/java/org/wise/portal/presentation/web/controllers/author/project/TranslateProjectAPIController.java
@@ -1,8 +1,10 @@
 package org.wise.portal.presentation.web.controllers.author.project;
 
 import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 import org.wise.portal.domain.project.impl.ProjectImpl;
 import org.wise.portal.domain.user.User;
 import org.wise.portal.service.project.ProjectService;
@@ -39,14 +42,14 @@ public class TranslateProjectAPIController {
   @Autowired
   protected TranslateProjectService translateProjectService;
 
-  @Value("${aws.accessKeyId}")
+  @Value("${aws.accessKeyId:}")
   private String accessKey;
 
-  @Value("${aws.secretAccessKey}")
+  @Value("${aws.secretAccessKey:}")
   private String secretKey;
 
-  @Value("${aws.region}")
-  private Region region;
+  @Value("${aws.region:}")
+  private String region;
 
   @PostMapping("{projectId}/{locale}")
   protected void saveTranslations(Authentication auth,
@@ -58,50 +61,34 @@ public class TranslateProjectAPIController {
     }
   }
 
-  @PostMapping("translationSuggestions")
-  protected String getSuggestedTranslation(Authentication auth, @RequestBody ObjectNode objectNode) throws IOException, IllegalArgumentException {
-    String srcLang = objectNode.get("srcLang").asText();
-    String targetLang = objectNode.get("targetLang").asText(); 
-    String srcText = objectNode.get("srcText").asText(); 
-    String srcLangCode = this.convertLanguageToAWSCode(srcLang);
-    String targetLangCode = this.convertLanguageToAWSCode(targetLang);
-    TranslateClient translateClient = buildTranslateClient();
-    TranslateTextRequest request = buildTranslateTextRequest(srcText, srcLangCode, targetLangCode);
-    TranslateTextResponse textResponse = translateClient.translateText(request);
-    return textResponse.translatedText();
+  @PostMapping("suggest")
+  protected String getSuggestedTranslation(Authentication auth, @RequestBody TranslatableText translatableText) throws IOException, IllegalArgumentException {
+    if (accessKey.equals("") || secretKey.equals("") || region.equals("")) {
+      throw new ResponseStatusException(
+        HttpStatus.INTERNAL_SERVER_ERROR,
+        "Missing application properties necessary for AWS Translate"
+      );
+    } else {
+      TranslateClient translateClient = buildTranslateClient();
+      TranslateTextRequest request = buildTranslateTextRequest(translatableText);
+      TranslateTextResponse textResponse = translateClient.translateText(request);
+      return textResponse.translatedText();
+    }
   }
 
   private TranslateClient buildTranslateClient() {
     AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
     return TranslateClient.builder()
-                .region(region)
+                .region(Region.of(region))
                 .credentialsProvider(StaticCredentialsProvider.create(credentials))
                 .build();
   }
 
-  private TranslateTextRequest buildTranslateTextRequest(String srcText, String srcLangCode,
-      String targetLangCode) {
+  private TranslateTextRequest buildTranslateTextRequest(TranslatableText translatableText) {
     return TranslateTextRequest.builder()
-                .text(srcText)
-                .sourceLanguageCode(srcLangCode)
-                .targetLanguageCode(targetLangCode)
+                .text(translatableText.getSrcText())
+                .sourceLanguageCode(translatableText.getSrcLangCode())
+                .targetLanguageCode(translatableText.getTargetLangCode())
                 .build();
   }
-
-  private String convertLanguageToAWSCode(String language) throws IllegalArgumentException {
-    return switch (language) {
-      case "English" -> "en";
-      case "Spanish" -> "es-MX";
-      case "Italian" -> "it";
-      case "Japanese" -> "ja";
-      case "German" -> "de";
-      case "Chinese (Simplified)" -> "zh";
-      case "Chinese (Traditional)" -> "zh-TW";
-      case "Dutch" -> "nl";
-      case "Korean" -> "ko";
-      case "Vietnamese" -> "vi";
-      default -> throw new IllegalArgumentException("Invalid language provided");
-    };
-  }
-  
 }
