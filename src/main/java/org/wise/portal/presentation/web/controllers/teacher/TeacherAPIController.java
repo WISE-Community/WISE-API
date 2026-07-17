92e45aba109fd0ee30506fa441a7b151b41533b4
Aaron Detre <aarondetre@gmail.com>
2026-07-17 09:56:23 -0700

    Move teacher registration into separate class

--- src/main/java/org/wise/portal/presentation/web/controllers/teacher/TeacherAPIController.java
+++ src/main/java/org/wise/portal/presentation/web/controllers/teacher/TeacherAPIController.java
@@ -4,24 +4,19 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
-import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
 
-import javax.mail.MessagingException;
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.acls.model.Permission;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,13 +27,8 @@ import org.wise.portal.domain.authentication.impl.TeacherUserDetails;
 import org.wise.portal.domain.group.Group;
 import org.wise.portal.domain.run.Run;
 import org.wise.portal.domain.user.User;
-import org.wise.portal.domain.usertag.UserTag;
-import org.wise.portal.presentation.web.controllers.ControllerUtil;
 import org.wise.portal.presentation.web.controllers.user.UserAPIController;
-import org.wise.portal.presentation.web.exception.InvalidNameException;
-import org.wise.portal.presentation.web.response.ResponseEntityGenerator;
 import org.wise.portal.presentation.web.response.SimpleResponse;
-import org.wise.portal.service.authentication.DuplicateUsernameException;
 import org.wise.portal.service.authentication.UserDetailsService;
 import org.wise.portal.service.usertags.UserTagsService;
 
@@ -127,105 +117,6 @@ public class TeacherAPIController extends UserAPIController {
   @GetMapping("/usernames")
   List<String> getAllTeacherUsernames() {
     return userDetailsService.retrieveAllTeacherUsernames();
-  }
-
-  @PostMapping("/register")
-  @Secured({ "ROLE_ANONYMOUS" })
-  ResponseEntity<Map<String, Object>> createTeacherAccount(
-      @RequestBody Map<String, String> teacherFields, HttpServletRequest request)
-      throws DuplicateUsernameException, InvalidNameException {
-    if (ControllerUtil.isReCaptchaEnabled()) {
-      String token = teacherFields.get("token");
-      if (!ControllerUtil.isReCaptchaResponseValid(token)) {
-        return ResponseEntityGenerator.createError("recaptchaResponseInvalid");
-      }
-    }
-    TeacherUserDetails tud = new TeacherUserDetails();
-    String firstName = teacherFields.get("firstName");
-    String lastName = teacherFields.get("lastName");
-    if (!isFirstNameAndLastNameValid(firstName, lastName)) {
-      String messageCode = this.getInvalidNameMessageCode(firstName, lastName);
-      throw new InvalidNameException(messageCode);
-    }
-    tud.setFirstname(firstName);
-    tud.setLastname(lastName);
-    String email = teacherFields.get("email");
-    tud.setEmailAddress(email);
-    tud.setCity(teacherFields.get("city"));
-    tud.setState(teacherFields.get("state"));
-    tud.setCountry(teacherFields.get("country"));
-    String googleUserId = teacherFields.get("googleUserId");
-    String microsoftUserId = teacherFields.get("microsoftUserId");
-    if (isSet(googleUserId)) {
-      tud.setGoogleUserId(googleUserId);
-      tud.setPassword(RandomStringUtils.random(10, true, true));
-    } else if (isSet(microsoftUserId)) {
-      tud.setMicrosoftUserId(microsoftUserId);
-      tud.setPassword(RandomStringUtils.random(10, true, true));
-    } else {
-      String password = teacherFields.get("password");
-      if (!passwordService.isValid(password)) {
-        return ResponseEntityGenerator.createError(passwordService.getErrors(password));
-      } else {
-        tud.setPassword(password);
-      }
-    }
-    String displayName = firstName + " " + lastName;
-    tud.setDisplayname(displayName);
-    tud.setEmailValid(true);
-    tud.setSchoollevel(Schoollevel.valueOf(teacherFields.get("schoolLevel")));
-    tud.setSchoolname(teacherFields.get("schoolName"));
-    tud.setHowDidYouHearAboutUs(teacherFields.get("howDidYouHearAboutUs"));
-    Locale locale = request.getLocale();
-    tud.setLanguage(locale.getLanguage());
-    User createdUser = this.userService.createUser(tud);
-    String username = createdUser.getUserDetails().getUsername();
-    String sendEmailEnabledStr = appProperties.getProperty("send_email_enabled", "false");
-    Boolean iSendEmailEnabled = Boolean.valueOf(sendEmailEnabledStr);
-    boolean socialAccount = this.isSet(googleUserId) || this.isSet(microsoftUserId);
-    if (iSendEmailEnabled) {
-      sendCreateTeacherAccountEmail(email, displayName, username, socialAccount, locale, request);
-    }
-    return createRegisterSuccessResponse(username);
-  }
-
-  private void sendCreateTeacherAccountEmail(String email, String displayName, String username,
-      boolean socialAccount, Locale locale, HttpServletRequest request) {
-    String fromEmail = appProperties.getProperty("portalemailaddress");
-    String[] recipients = { email };
-    String defaultSubject = messageSource.getMessage(
-        "presentation.web.controllers.teacher.registerTeacherController.welcomeTeacherEmailSubject",
-        null, Locale.US);
-    String subject = messageSource.getMessage(
-        "presentation.web.controllers.teacher.registerTeacherController.welcomeTeacherEmailSubject",
-        null, defaultSubject, locale);
-    String defaultBody = messageSource.getMessage(
-        "presentation.web.controllers.teacher.registerTeacherController.welcomeTeacherEmailBody",
-        new Object[] { username }, Locale.US);
-    String gettingStartedUrl = getGettingStartedUrl(request);
-    String message;
-    if (socialAccount) {
-      message = messageSource.getMessage(
-          "presentation.web.controllers.teacher.registerTeacherController.welcomeTeacherEmailBodyNoUsername",
-          new Object[] { displayName, gettingStartedUrl }, defaultBody, locale);
-    } else {
-      message = messageSource.getMessage(
-          "presentation.web.controllers.teacher.registerTeacherController.welcomeTeacherEmailBody",
-          new Object[] { displayName, username, gettingStartedUrl }, defaultBody, locale);
-    }
-    try {
-      mailService.postMail(recipients, subject, message, fromEmail);
-    } catch (MessagingException e) {
-      e.printStackTrace();
-    }
-  }
-
-  private String getGettingStartedUrl(HttpServletRequest request) {
-    return ControllerUtil.getPortalUrlString(request) + "/help/getting-started";
-  }
-
-  private boolean isSet(String value) {
-    return value != null && !value.isEmpty();
   }
 
   private List<HashMap<String, Object>> getRunSharedOwnersList(Run run) {
---
+++ src/test/java/org/wise/portal/presentation/web/controllers/teacher/TeacherRegistrationAPIControllerTest.java
@@ -0,0 +1,77 @@
+package org.wise.portal.presentation.web.controllers.teacher;
+
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.isA;
+import static org.easymock.EasyMock.replay;
+import static org.easymock.EasyMock.verify;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+import org.easymock.EasyMockExtension;
+import org.easymock.Mock;
+import org.easymock.TestSubject;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.wise.portal.domain.authentication.impl.TeacherUserDetails;
+import org.wise.portal.presentation.web.controllers.APIControllerTest;
+import org.wise.portal.presentation.web.exception.InvalidNameException;
+import org.wise.portal.service.authentication.DuplicateUsernameException;
+import org.wise.portal.service.authentication.UserDetailsService;
+import org.wise.portal.service.password.impl.PasswordServiceImpl;
+import org.wise.portal.service.usertags.UserTagsService;
+
+@ExtendWith(EasyMockExtension.class)
+public class TeacherRegistrationAPIControllerTest extends APIControllerTest {
+
+  @TestSubject
+  private TeacherRegistrationAPIController teacherRegistrationAPIController = new TeacherRegistrationAPIController();
+
+  @Mock
+  private UserDetailsService userDetailsService;
+
+  @Mock
+  private UserTagsService userTagsService;
+
+  @Test
+  public void createTeacherAccount_InvalidPassword_ReturnError()
+      throws DuplicateUsernameException, InvalidNameException {
+    HashMap<String, String> teacherFields = createDefaultTeacherFields();
+    teacherFields.put("password", PasswordServiceImpl.INVALID_PASSWORD_TOO_SHORT);
+    ResponseEntity<Map<String, Object>> response = teacherRegistrationAPIController
+        .createTeacherAccount(teacherFields, request);
+    assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    assertEquals("invalidPassword", response.getBody().get("messageCode"));
+  }
+
+  @Test
+  public void createTeacherAccount_WithGoogleUserId_CreateUser()
+      throws DuplicateUsernameException, InvalidNameException {
+    HashMap<String, String> teacherFields = createDefaultTeacherFields();
+    teacherFields.put("googleUserId", "123456789");
+    expect(request.getLocale()).andReturn(Locale.US);
+    replay(request);
+    expect(userService.createUser(isA(TeacherUserDetails.class))).andReturn(teacher1);
+    replay(userService);
+    ResponseEntity<Map<String, Object>> response = teacherRegistrationAPIController
+        .createTeacherAccount(teacherFields, request);
+    assertEquals(TEACHER_USERNAME, response.getBody().get("username"));
+    verify(request);
+    verify(userService);
+  }
+
+  private HashMap<String, String> createDefaultTeacherFields() {
+    HashMap<String, String> fields = new HashMap<String, String>();
+    fields.put("firstName", TEACHER_FIRSTNAME);
+    fields.put("lastName", TEACHER_LASTNAME);
+    fields.put("schoolLevel", "COLLEGE");
+    fields.put("birthMonth", "1");
+    fields.put("birthDay", "1");
+    fields.put("gender", "MALE");
+    return fields;
+  }
+}
