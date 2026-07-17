907d6836eb5f95cb23e0fa88e878923b54eea9ef
Aaron Detre <aarondetre@gmail.com>
2026-07-17 09:58:48 -0700

    Refactor teacher registration

--- src/main/java/org/wise/portal/presentation/web/controllers/teacher/TeacherRegistrationAPIController.java
+++ src/main/java/org/wise/portal/presentation/web/controllers/teacher/TeacherRegistrationAPIController.java
@@ -18,110 +18,155 @@ import org.wise.portal.domain.authentication.impl.TeacherUserDetails;
 import org.wise.portal.domain.user.User;
 import org.wise.portal.presentation.web.controllers.ControllerUtil;
 import org.wise.portal.presentation.web.exception.InvalidNameException;
+import org.wise.portal.presentation.web.exception.InvalidPasswordException;
+import org.wise.portal.presentation.web.exception.RecaptchaVerificationException;
 import org.wise.portal.presentation.web.response.ResponseEntityGenerator;
 import org.wise.portal.service.authentication.DuplicateUsernameException;
-
 
 @RestController
 @RequestMapping("/api/teacher/register")
 public class TeacherRegistrationAPIController extends TeacherAPIController {
 
+  private final String emailCodePrefix = 
+    "presentation.web.controllers.teacher.registerTeacherController.welcomeTeacherEmail";
+  private final String welcomeBodyCode = this.emailCodePrefix + "Body";
+  private final String welcomeSocialAccountBodyCode = this.emailCodePrefix + "BodyNoUsername";
+  private final String welcomeSubjectCode = this.emailCodePrefix + "Subject";
+ 
   @PostMapping()
   @Secured({ "ROLE_ANONYMOUS" })
   ResponseEntity<Map<String, Object>> createTeacherAccount(
       @RequestBody Map<String, String> teacherFields, HttpServletRequest request)
       throws DuplicateUsernameException, InvalidNameException {
-    if (ControllerUtil.isReCaptchaEnabled()) {
-      String token = teacherFields.get("token");
-      if (!ControllerUtil.isReCaptchaResponseValid(token)) {
-        return ResponseEntityGenerator.createError("recaptchaResponseInvalid");
-      }
+    try {
+      validateTeacherFields(teacherFields);
+    } catch (RecaptchaVerificationException e) {
+      return ResponseEntityGenerator.createError("recaptchaResponseInvalid");
+    } catch (InvalidPasswordException e) {
+      return ResponseEntityGenerator.createError(
+        passwordService.getErrors(teacherFields.get("password")));
     }
-    TeacherUserDetails tud = new TeacherUserDetails();
-    String firstName = teacherFields.get("firstName");
-    String lastName = teacherFields.get("lastName");
-    if (!isFirstNameAndLastNameValid(firstName, lastName)) {
-      String messageCode = this.getInvalidNameMessageCode(firstName, lastName);
-      throw new InvalidNameException(messageCode);
-    }
-    tud.setFirstname(firstName);
-    tud.setLastname(lastName);
-    String email = teacherFields.get("email");
-    tud.setEmailAddress(email);
-    tud.setCity(teacherFields.get("city"));
-    tud.setState(teacherFields.get("state"));
-    tud.setCountry(teacherFields.get("country"));
-    String googleUserId = teacherFields.get("googleUserId");
-    String microsoftUserId = teacherFields.get("microsoftUserId");
-    if (isSet(googleUserId)) {
-      tud.setGoogleUserId(googleUserId);
-      tud.setPassword(RandomStringUtils.random(10, true, true));
-    } else if (isSet(microsoftUserId)) {
-      tud.setMicrosoftUserId(microsoftUserId);
-      tud.setPassword(RandomStringUtils.random(10, true, true));
-    } else {
-      String password = teacherFields.get("password");
-      if (!passwordService.isValid(password)) {
-        return ResponseEntityGenerator.createError(passwordService.getErrors(password));
-      } else {
-        tud.setPassword(password);
-      }
-    }
-    String displayName = firstName + " " + lastName;
-    tud.setDisplayname(displayName);
-    tud.setEmailValid(true);
-    tud.setSchoollevel(Schoollevel.valueOf(teacherFields.get("schoolLevel")));
-    tud.setSchoolname(teacherFields.get("schoolName"));
-    tud.setHowDidYouHearAboutUs(teacherFields.get("howDidYouHearAboutUs"));
     Locale locale = request.getLocale();
-    tud.setLanguage(locale.getLanguage());
+    TeacherUserDetails tud = createTeacherUserDetails(teacherFields, locale);
     User createdUser = this.userService.createUser(tud);
     String username = createdUser.getUserDetails().getUsername();
-    String sendEmailEnabledStr = appProperties.getProperty("send_email_enabled", "false");
-    Boolean iSendEmailEnabled = Boolean.valueOf(sendEmailEnabledStr);
-    boolean socialAccount = this.isSet(googleUserId) || this.isSet(microsoftUserId);
-    if (iSendEmailEnabled) {
-      sendCreateTeacherAccountEmail(email, displayName, username, socialAccount, locale, request);
+    if (isSendEmailEnabled()) {
+      boolean socialAccount = isSet(tud.getGoogleUserId()) || isSet(tud.getMicrosoftUserId());
+      sendWelcomeTeacherEmail(tud.getEmailAddress(), tud.getDisplayname(), username, 
+                              socialAccount, locale, request);
     }
     return createRegisterSuccessResponse(username);
   }
 
-  private void sendCreateTeacherAccountEmail(String email, String displayName, String username,
-      boolean socialAccount, Locale locale, HttpServletRequest request) {
-    String fromEmail = appProperties.getProperty("portalemailaddress");
-    String[] recipients = { email };
-    String defaultSubject = messageSource.getMessage(
-        "presentation.web.controllers.teacher.registerTeacherController.welcomeTeacherEmailSubject",
-        null, Locale.US);
-    String subject = messageSource.getMessage(
-        "presentation.web.controllers.teacher.registerTeacherController.welcomeTeacherEmailSubject",
-        null, defaultSubject, locale);
-    String defaultBody = messageSource.getMessage(
-        "presentation.web.controllers.teacher.registerTeacherController.welcomeTeacherEmailBody",
-        new Object[] { username }, Locale.US);
+  private Boolean isSendEmailEnabled() {
+    String sendEmailEnabledStr = appProperties.getProperty("send_email_enabled", "false");
+    return Boolean.valueOf(sendEmailEnabledStr);
+  }
+
+  private void sendWelcomeTeacherEmail(String email, String displayName, String username,
+                                       boolean socialAccount, Locale locale, 
+                                       HttpServletRequest request) {
+    String subject = getEmailMessage(this.welcomeSubjectCode, this.welcomeSubjectCode, null, locale);
+    String body = getWelcomeTeacherBody(displayName, username, socialAccount, locale, request);
+    this.sendEmail(email, subject, body);
+  }
+
+  private String getEmailMessage(String defaultCode, String code, Object[] args, Locale locale) {
+    String defaultMessage = messageSource.getMessage(defaultCode, args, Locale.US);
+    return messageSource.getMessage(code, args, defaultMessage, locale);
+  }
+
+  private String getWelcomeTeacherBody(String displayName, String username, boolean socialAccount,
+                                       Locale locale, HttpServletRequest request) {
     String gettingStartedUrl = getGettingStartedUrl(request);
-    String message;
-    if (socialAccount) {
-      message = messageSource.getMessage(
-          "presentation.web.controllers.teacher.registerTeacherController.welcomeTeacherEmailBodyNoUsername",
-          new Object[] { displayName, gettingStartedUrl }, defaultBody, locale);
-    } else {
-      message = messageSource.getMessage(
-          "presentation.web.controllers.teacher.registerTeacherController.welcomeTeacherEmailBody",
-          new Object[] { displayName, username, gettingStartedUrl }, defaultBody, locale);
-    }
-    try {
-      mailService.postMail(recipients, subject, message, fromEmail);
-    } catch (MessagingException e) {
-      e.printStackTrace();
-    }
+    String code = socialAccount ? this.welcomeSocialAccountBodyCode : this.welcomeBodyCode;
+    Object[] args = socialAccount 
+      ? new Object[] { displayName, gettingStartedUrl } 
+      : new Object[] { displayName, username, gettingStartedUrl };
+    return getEmailMessage(this.welcomeBodyCode, code, args, locale);
   }
 
   private String getGettingStartedUrl(HttpServletRequest request) {
     return ControllerUtil.getPortalUrlString(request) + "/help/getting-started";
   }
 
+  private void sendEmail(String email, String subject, String body) {
+    String fromEmail = appProperties.getProperty("portalemailaddress");
+    String[] recipients = { email };
+    try {
+      mailService.postMail(recipients, subject, body, fromEmail);
+    } catch (MessagingException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private void validateTeacherFields(Map<String, String> teacherFields) 
+    throws RecaptchaVerificationException, InvalidNameException, InvalidPasswordException {
+    validateReCaptcha(teacherFields.get("token"));
+    validateFirstAndLastName(teacherFields.get("firstName"), teacherFields.get("lastName"));
+    validatePassword(teacherFields.get("password"));
+  }
+
+  private void validateReCaptcha(String token) throws RecaptchaVerificationException {
+    if (ControllerUtil.isReCaptchaEnabled()) {
+      if (!ControllerUtil.isReCaptchaResponseValid(token)) {
+        throw new RecaptchaVerificationException("Invalid ReCaptcha Response");
+      }
+    }
+  }
+
+  private void validateFirstAndLastName(String firstName, String lastName) 
+    throws InvalidNameException {
+    if (!isFirstNameAndLastNameValid(firstName, lastName)) {
+      String messageCode = this.getInvalidNameMessageCode(firstName, lastName);
+      throw new InvalidNameException(messageCode);
+    }
+  }
+
+  private void validatePassword(String password) throws InvalidPasswordException {
+    if (password != null && !passwordService.isValid(password)) {
+      throw new InvalidPasswordException();
+    }
+  }
+
+  private TeacherUserDetails createTeacherUserDetails(Map<String, String> teacherFields, 
+                                                      Locale locale) {
+    TeacherUserDetails tud = new TeacherUserDetails();
+    tud.setFirstname(teacherFields.get("firstName"));
+    tud.setLastname(teacherFields.get("lastName"));
+    tud.setEmailAddress(teacherFields.get("email"));
+    tud.setCity(teacherFields.get("city"));
+    tud.setState(teacherFields.get("state"));
+    tud.setCountry(teacherFields.get("country"));
+    tud.setDisplayname(tud.getFirstname() + " " + tud.getLastname());
+    tud.setSchoollevel(Schoollevel.valueOf(teacherFields.get("schoolLevel")));
+    tud.setSchoolname(teacherFields.get("schoolName"));
+    tud.setHowDidYouHearAboutUs(teacherFields.get("howDidYouHearAboutUs"));
+    tud.setLanguage(locale.getLanguage());
+    setPassword(teacherFields, tud);
+    tud.setEmailValid(true);
+    return tud;
+  }
+
+  private void setPassword(Map<String, String> teacherFields, TeacherUserDetails tud) {
+    String googleUserId = teacherFields.get("googleUserId");
+    String microsoftUserId = teacherFields.get("microsoftUserId");
+    if (isSet(googleUserId)) {
+      tud.setGoogleUserId(googleUserId);
+      setRandomPassword(tud);
+    } else if (isSet(microsoftUserId)) {
+      tud.setMicrosoftUserId(microsoftUserId);
+      setRandomPassword(tud);
+    } else {
+      tud.setPassword(teacherFields.get("password"));
+    }
+  }
+
   private boolean isSet(String value) {
     return value != null && !value.isEmpty();
+  }
+
+  private void setRandomPassword(TeacherUserDetails tud) {
+    tud.setPassword(RandomStringUtils.random(10, true, true));
   }
 }
